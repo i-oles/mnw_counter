5776b00e101c65cc19904e65b7c3b097f03ceab4
i-oles@github.com <ioles@Igors-MacBook-Pro.local>
2021-02-14 18:48:22 +0100

    process in background fixed, refactored some vars

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import QMessageBox, QMainWindow
 from PyQt5.QtCore import Qt
-from string_operations import StringOperations
+from string_operations import StringOperations, ReportFile
 from gui import Ui_MainWindow
 from about_widget import Ui_widget
 import os
@@ -9,11 +9,9 @@ import re
 import sys
 from datetime import date
 
-#todo uncheck count and list cBox
 #todo logo
-#todo uncheck tiff bug: popup or unable check
 #todo center app on screen
-#todo correct count method - separate couting from settings
+#todo fix progress bar
 
 
 class Ui_AboutWindow(Ui_widget, QMainWindow):
@@ -31,8 +29,8 @@ class IzzyCounterWindow(Ui_MainWindow, QMainWindow):
         self.char_1 = ','
         self.char_2 = '-'
         self.report_dir = 'Daily_Reports'
-        self.tiff_extension = 'tiff'
-        self.jpg_extension = 'jpg'
+        self.tiff_ext = 'tiff'
+        self.jpg_ext = 'jpg'
 
         self.current_date = date.today()
         self.current_date.strftime("%d-%m-%Y")
@@ -40,85 +38,25 @@ class IzzyCounterWindow(Ui_MainWindow, QMainWindow):
         self.logo = 'app_logo.jpg'
         self.centralwidget.setWindowIcon(QtGui.QIcon(self.logo))
 
+        self.second_window = Ui_AboutWindow(self)
+
+        # *** default app settings ***
         self.cBoxList.setChecked(True)
         self.cBoxCount.setChecked(True)
         self.cBoxTiff.setChecked(True)
         self.btnCount.setDisabled(True)
         self.exportResults.setDisabled(True)
 
+        # *** widgets connections ***
         self.cBoxTiff.stateChanged.connect(self.uncheck_another_cBox)
         self.cBoxJpg.stateChanged.connect(self.uncheck_another_cBox)
-
         self.lineEdit.textChanged.connect(self.unlock_btn_count)
-
         self.btnBrowse.clicked.connect(self.browse_dir)
         self.btnCount.clicked.connect(self.when_btnCount_clicked)
         self.menuFile.triggered.connect(self.export_to_file)
         self.menuAbout.triggered.connect(self.show_about_IzzyCounter)
-        self.second_window = Ui_AboutWindow(self)
-
         self.menuFile.triggered.connect(
-            lambda: self.show_popup(f"Your results was successfully exported to folder '{self.report_dir}' in your images folder"))
-
-    def when_btnCount_clicked(self):
-        self.clear_widgets_after_clicked()
-        self.check_file_extention(self.tiff_extension, self.jpg_extension)
-        self.check_is_lineEdit_empty()
-        self.is_manual_path_typing_correct()
-        self.boxes_not_checked()
-        if self.dir_path_ok:
-            self.find_all_ones_from_dir()
-            self.make_singles_and_set_list()
-            self.counting_progress()
-            self.display_count_result()
-            self.display_item_lists()
-            self.allow_export_results_after_count()
-
-    def display_count_result(self):
-        if self.cBoxCount.isChecked():
-            self.count_result = str(len(self.singles_long))
-            self.labelResult.setText(self.count_result)
-
-    def display_item_lists(self):
-        if self.cBoxList.isChecked():
-            self.list_display(self.singles_long, self.listWidgetSingles)
-            self.list_display(self.sets_long, self.listWidgetSets)
-
-    def show_about_IzzyCounter(self):
-        self.second_window.show()
-
-    def unlock_btn_count(self):
-        if len(self.lineEdit.text()) > 0:
-            self.btnCount.setDisabled(False)
-
-    def browse_dir(self):
-        self.directory = str(QtWidgets.QFileDialog.getExistingDirectory())
-        self.lineEdit.setText(f'{self.directory}')
-
-    def clear_widgets_after_clicked(self):
-        self.progressBar.setValue(0)
-        self.labelResult.clear()
-        self.listWidgetSingles.clear()
-        self.listWidgetSets.clear()
-
-    def is_manual_path_typing_correct(self):
-        self.directory = str(self.lineEdit.text())
-        self.dir_path_ok = os.path.isdir(self.directory)
-        if self.lineEdit.text():
-            if not self.dir_path_ok:
-                self.show_popup('Path does not exist! Please type or choose correct path!')
-
-    def check_is_lineEdit_empty(self):
-        if not self.lineEdit.text():
-            self.show_popup('Please choose directory path!')
-
-    def boxes_not_checked(self):
-        if not self.cBoxCount.isChecked() and not self.cBoxList.isChecked():
-            self.show_popup('Please choose one of the following display options')
-
-    def allow_export_results_after_count(self):
-        if self.listWidgetSingles.count() > 0 or len(self.labelResult.text()) > 0:
-            self.exportResults.setDisabled(False)
+            lambda: self.__show_popup(f"Your results was successfully exported to folder '{self.report_dir}' in your images folder"))
 
     def uncheck_another_cBox(self, state):
         if state == Qt.Checked:
@@ -127,17 +65,59 @@ class IzzyCounterWindow(Ui_MainWindow, QMainWindow):
             else:
                 self.cBoxTiff.setChecked(False)
 
+    def unlock_btn_count(self):
+        if len(self.lineEdit.text()) > 0:
+            self.btnCount.setDisabled(False)
+
+    # *** setting up directory path using btnBrowse ***
+    def browse_dir(self):
+        self.directory = str(QtWidgets.QFileDialog.getExistingDirectory())
+        self.lineEdit.setText(f'{self.directory}')
+
+    #todo *** resetting all widget before counting,
+    def when_btnCount_clicked(self):
+        self.clear_widgets_after_clicked()
+        self.check_file_extention(self.tiff_ext, self.jpg_ext)
+        self.check_is_empty(self.lineEdit, self.__show_popup, 'Please choose directory path!')
+        self.is_manual_path_typing_correct()
+        if self.dir_path_ok:
+            self.cBoxes_unchecked(self.cBoxTiff, self.cBoxJpg, self.__show_popup, 'Please choose file extension!')
+            if self.cBoxJpg.isChecked() or self.cBoxTiff.isChecked():
+                self.cBoxes_unchecked(self.cBoxCount, self.cBoxList, self.__show_popup, 'Please choose display option!')
+                if self.dir_path_ok and (self.cBoxCount.isChecked() or self.cBoxList.isChecked()):
+                    self.find_all_ones_from_dir()
+                    self.make_singles_and_set_list()
+                    self.counting_progress()
+                    self.display_count_result()
+                    self.display_item_lists()
+                    self.allow_export_results_after_count()
+
+    def clear_widgets_after_clicked(self):
+        self.progressBar.setValue(0)
+        self.labelResult.clear()
+        self.listWidgetSingles.clear()
+        self.listWidgetSets.clear()
+
     def check_file_extention(self, ext_1, ext_2):
         if self.cBoxTiff.isChecked():
             self.file_ext = ext_1
         else:
             self.file_ext = ext_2
 
-    def show_popup(self, text):
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-        msg.setText(text)
-        msg.exec_()
+    def check_is_empty(self, widget, func, message):
+        if not widget.text():
+            func(message)
+
+    def is_manual_path_typing_correct(self):
+        self.directory = str(self.lineEdit.text())
+        self.dir_path_ok = os.path.isdir(self.directory)
+        if self.lineEdit.text():
+            if not self.dir_path_ok:
+                self.__show_popup('Path does not exist! Please type or choose correct path!')
+
+    def cBoxes_unchecked(self, cBox1, cBox2, func, message):
+        if not cBox1.isChecked() and not cBox2.isChecked():
+            func(message)
 
     def find_all_ones_from_dir(self):
         self.ones_all = []
@@ -148,7 +128,10 @@ class IzzyCounterWindow(Ui_MainWindow, QMainWindow):
                 if first_file:
                     self.ones_all.append(file)
 
+    # *** here all files ended with (1) from provided directory
     def make_singles_and_set_list(self):
+        self.singles_long = []
+
         ones_splited = list(StringOperations.split_duets(self.ones_all, self.char_1, self.component))
         ones_without_mnw = [StringOperations.cut_char(item, self.component) for item in ones_splited]
         ones_unique = list(set(ones_without_mnw))
@@ -158,7 +141,6 @@ class IzzyCounterWindow(Ui_MainWindow, QMainWindow):
         singles_short = list(set(ones_short))
         ones_dict_keys = ones_dict.keys()
 
-        self.singles_long = []
         self.sets_long = ones_unique.copy()
         for item in singles_short:
             if item in ones_dict_keys:
@@ -171,31 +153,60 @@ class IzzyCounterWindow(Ui_MainWindow, QMainWindow):
         for percent in range(101):
             self.progressBar.setValue(percent)
 
-    def list_display(self, list, widget):
+    def display_count_result(self):
+        if self.cBoxCount.isChecked():
+            self.count_result = str(len(self.singles_long))
+            self.labelResult.setText(self.count_result)
+
+    def display_item_lists(self):
+        if self.cBoxList.isChecked():
+            self.__list_display(self.singles_long, self.listWidgetSingles)
+            self.__list_display(self.sets_long, self.listWidgetSets)
+
+    def allow_export_results_after_count(self):
+        if self.listWidgetSingles.count() > 0 or len(self.labelResult.text()) > 0:
+            self.exportResults.setDisabled(False)
+
+    def show_about_IzzyCounter(self):
+        self.second_window.show()
+
+    def export_to_file(self):
+        self.make_dir_if_not_exist(self.directory, self.report_dir)
+
+        one_empty_line = '\n'
+        two_empty_lines = '\n\n'
+
+        result_file = open(f'{self.directory}/{self.report_dir}/{self.current_date}.txt', 'w')
+
+        date_line = f'Date: {self.current_date}{one_empty_line}'
+        counting_result_line = f'You had taken pictures of {self.count_result} objects.{one_empty_line}'
+        label_singles = f'{self.labelPhotographed.text()}'
+
+        result_file.writelines(self.__list_format([date_line, counting_result_line, label_singles]))
+        result_file.writelines(self.__list_format(self.singles_long))
+        result_file.write(two_empty_lines)
+        if self.listWidgetSets.count() != 0:
+            label_sets = f'{self.labelListSets.text()} {one_empty_line}'
+            result_file.write(label_sets)
+            result_file.writelines(self.__list_format(self.sets_long))
+        result_file.close()
+
+    def make_dir_if_not_exist(self, path, dir_name):
+        if not os.path.exists(f'{path}/{dir_name}'):
+            os.mkdir(f'{path}/{dir_name}')
+
+    def __list_display(self, list, widget):
         for num, item in enumerate(list):
             widget.insertItem(num, item)
 
-    def export_to_file(self):
-        if not os.path.exists(f'{self.directory}/{self.report_dir}'):
-            os.mkdir(f'{self.directory}/{self.report_dir}')
+    def __list_format(self, some_list):
+        return [(x + '\n') for x in some_list]
 
-        def list_format(some_list):
-            return [(x + '\n') for x in some_list]
-
-        result_file = open(r'{}/{}/{}.txt'.format(self.directory, self.report_dir, self.current_date), 'w')
-        date_line = f'Date: {self.current_date} \n \n'
-        counter_line = f'You had taken pictures of {self.count_result} objects. \n \n'
-        label_singles = f'{self.labelPhotographed.text()} \n'
-        label_sets = f'{self.labelListSets.text()} \n'
-        result_file.write(date_line)
-        result_file.write(counter_line)
-        result_file.write(label_singles)
-        result_file.writelines(list_format(self.singles_long))
-        result_file.write('\n\n')
-        if not self.listWidgetSets.count() == 0:
-            result_file.write(label_sets)
-            result_file.writelines(list_format(self.sets_long))
-        result_file.close()
+    def __show_popup(self, text):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText(text)
+        msg.exec_()
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
--- string_operations.py
+++ string_operations.py
@@ -1,6 +1,49 @@
 from itertools import chain
 
 
+class ReportFile:
+    one_empty_line = '\n'
+    two_empty_lines = '\n\n'
+
+    @staticmethod
+    def create_new_file(path, dir_name, date):
+        result_file = open(f'{path}/{dir_name}/{date}.txt', 'w')
+        return result_file
+
+    @staticmethod
+    def add_date_line(date, space):
+        date_line = f'Date: {date}{space}'
+        return date_line
+
+    @staticmethod
+    def add_counting_result_line(num, space):
+        counting_result_line = f'You had taken pictures of {num} objects.{space}'
+        return counting_result_line
+
+    @staticmethod
+    def add_label_line(label):
+        label_singles = f'{label.text()}'
+        return label_singles
+
+    @staticmethod
+    def write_lines(file, func, line):
+        file.writelines(func(line))
+
+    """
+        result_file.writelines(self.__list_format(self.singles_long))
+        result_file.write(two_empty_lines)
+        if self.listWidgetSets.count() != 0:
+            label_sets = f'{self.labelListSets.text()} {one_empty_line}'
+            result_file.write(label_sets)
+            result_file.writelines(self.__list_format(self.sets_long))
+        result_file.close()
+    """
+    @staticmethod
+    def create_new_result_file(path, result_dir_name, current_date):
+        result_file = open(r'{}/{}/{}.txt'.format(path, result_dir_name, current_date), 'w')
+        return result_file
+
+
 class StringOperations:
     @staticmethod
     def split_duets(start_list, char, component):
