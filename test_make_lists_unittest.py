8a092fce04e5276be718f3e8c99168641f00391a
Igor Oleś <68300324+i-oles@users.noreply.github.com>
2021-03-05 14:04:31 +0100

    Update test_make_lists_unittest.py

--- test_make_lists_unittest.py
+++ test_make_lists_unittest.py
@@ -1,41 +1,28 @@
-#import unittest
-#from main import IzzyCounterWindow
-import os
+import unittest
+from PyQt5.QtWidgets import QMainWindow
+from main import IzzyCounterWindow
 
-"""
-class TestMakeLists(unittest.TestCase):
-    def test_make_lists(self):
-        self.files_for_test = ['szm123_1mnw(1).tiff',
-                               'szm123_2mnw(1).tiff',
-                               'szm123_1-2mnw(1).tiff',
-                               'szm111_a-cmnw!a(1).tiff',
-                               'szm111_a-cmnw!b(1).tiff',
-                               'szm111_a-cmnw(1).tiff'
-                               ]
+# todo fix test
+
+class TestMakeItemsList(unittest.TestCase):
+    def setUp(self):
+        self.configs = IzzyCounterWindow(QMainWindow)
+
+        self.ones_all = ['szm123_1mnw(1).tiff',
+                         'szm123_2mnw(1).tiff',
+                         'szm123_1-2mnw(1).tiff',
+                         'szm111_a-cmnw!a(1).tiff',
+                         'szm111_a-cmnw!b(1).tiff',
+                         'szm111_a-cmnw(1).tiff'
+                         ]
+
+    def test_make_singles_and_set_list(self):
+        self.configs.make_singles_and_set_list()
 
         self.result_single = ['szm123_1', 'szm123_2', 'szm111_a-c']
 
-        self.assertEqual(IzzyCounterWindow.make_singles_and_set_list(self.files_for_test, self.result_single))
+        self.assertEqual(self.ones_all, self.result_single)
 
-
-
-class TestSum(unittest.TestCase):
-
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
 
 if __name__ == '__main__':
     unittest.main()
-
-"""
-
-dir = '/Users/ioles/Desktop/'
-
-if os.path.isfile(dir):
-    print('')
-
-if __name__ == '__main__':
-    pass
